ac06d0601186240e3e39f6a2e75ed5d9404fc2c2
SER515-Fall22-Team14 <112654680+SER515-Fall22-Team14@users.noreply.github.com>
2022-10-28 00:39:26 -0700

    Add files via upload

--- ICA8/Urinals.java
+++ ICA8/Urinals.java
@@ -1,13 +1,51 @@
-package org.example;
+ package org.example;
+import java.io.*;
 import java.util.Scanner;
 public class Urinals {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s;
-        System.out.println(" enter the input");
-        s=sc.nextLine();
-        int output= countUrinals(s);
-        System.out.println(output);
+        //System.out.println(" enter the input");
+        s = sc.nextLine();
+
+        //System.out.println(output);
+
+        System.out.println("enter option");
+        int opt = sc.nextInt();
+        if (opt == 1) {
+            int output = countUrinals(s);
+            System.out.println(output);
+        }
+        BufferedReader br = null;
+        else {
+            try {
+                FileReader file = new FileReader(new File("urinal.dat"));
+                br = new BufferedReader(file);
+                //int j=0, c=0;
+                File fout = new File("rule.txt");
+                if (!fout.exists()) {
+                    fout.createNewFile();
+
+                }
+
+                FileWriter fw = new FileWriter(fout.getAbsolutePath());
+                BufferedWriter bw = new BufferedWriter(fw);
+                while (true) {
+                    String temp = br.readLine();
+                    if (temp == null) break;
+                    int val = countUrinals(temp);
+                    //    System.out.println(val);
+                    bw.write(Integer.toString(val));
+                    bw.newLine();
+                }
+                br.close();
+                bw.close();
+            } catch (Exception e) {
+                System.out.println(e);
+                // i=getUrinals(null);
+            }
+        }
+
     }
     public static boolean goodString( String str )
     {  // checks to see if valid string
@@ -20,6 +58,7 @@ public class Urinals {
             }
             else {
                 gs=false;
+                break;
             }
         }
         return gs;
