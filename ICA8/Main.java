3843958218f3789dc08ad60101ac3bc38f2ea3ce
SER515-Fall22-Team14 <112654680+SER515-Fall22-Team14@users.noreply.github.com>
2022-10-27 22:16:56 -0700

    changes in code

--- ICA8/Main.java
+++ ICA8/Main.java
@@ -1,40 +1,46 @@
+package org.example;
 import java.util.Scanner;
 public class Main {
-            public static void main(String[] args){
-                int output= countUrinals();
-                System.out.println(output);
-            }
-            public static int countUrinals() {
-                String s;
-                int czero = 0, cone=0,count = 0;
-                Scanner sc = new Scanner(System.in);
-                System.out.println(" enter the input");
-                s=sc.nextLine();
-                char[] str= new char[s.length()];
-                for(int i=1;i<str.length;i++) {
-                    if(s.contains("11")) {
-                        System.out.println("string formatting error");
-                        return -1;
-                    }
-                }
-                for(int i=0;i< s.length();i++) {
-                    str[i]=s.charAt(i);
-                }
-                int i=0;
-                while(i<str.length){
-                    if(str[i]=='0')
-                        czero++;
-                    else {
-                        count = count + (czero-cone)/2;
-                        czero=0;
-                        cone=1;
-                    }
-                    i++;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s;
+        System.out.println(" enter the input");
+        s=sc.nextLine();
+        int output= countUrinals(s);
+        System.out.println(output);
+    }
+    public static int countUrinals(String s) {
 
-                }
-                count = count + czero/2;
-                return count;
+        int czero = 0, cone=0,count = 0;
+        char[] str= new char[s.length()];
+        for(int i=1;i<str.length;i++) {
+            if(s.contains("11")) {
+                System.out.println("string formatting error");
+                return -1;
             }
         }
+        if(!s.contains(("1"))){
+            return (s.length()+1)/2;
+        }
+        for(int i=0;i< s.length();i++) {
+            str[i]=s.charAt(i);
+        }
+        int i=0;
+        while(i<str.length){
+            if(str[i]=='0')
+                czero++;
+            else {
+                count = count + (czero-cone)/2;
+                czero=0;
+                cone=1;
+            }
+            i++;
+
+        }
+        count = count + czero/2;
+        return count;
+    }
+}
+
 
 
